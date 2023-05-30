ee184d96626a37080b0d46200e49bcb8e41dc8b1
Mustafa Cengar <126611512+cengarm@users.noreply.github.com>
2023-05-30 23:17:53 +0300

    Add files via upload

---
+++ entity/concretes/SystemManagement.java
@@ -0,0 +1,29 @@
+package com.example.hotelReservationSystem.entity.concretes;
+
+import com.example.hotelReservationSystem.entity.abstracts.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "system_management")
+@PrimaryKeyJoinColumn(name = "system_management_id", referencedColumnName = "user_id")
+public class SystemManagement extends User {
+
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+
+}
